9c37e4547cde0b89d1e6be07dbc02232a0c1bcce
Alan Johnson <alan@DESKTOP-U0LVIVT.localdomain>
2023-11-04 14:19:22 -0400

    AJJ-1330: Remove Next.js starter content and break site into seasonal sections for boilerplate

--- src/components/seasonal-effects/SeasonalSection.tsx
+++ src/components/seasonal-effects/SeasonalSection.tsx
@@ -15,6 +15,10 @@ export default function SeasonalSection({
   const [contentHeight, setContentHeight] = useState(0);
   const [contentWidth, setContentWidth] = useState(0);
 
+  // useLayoutEffect is used over useEffect because we need to measure the 
+  // dimensions of the DOM element as soon as it is rendered but before the 
+  // browser paints. This ensures that we have the exact measurements to use before 
+  // the season animations start.
   useLayoutEffect(() => {
     if (contentRef.current) {
       setContentHeight(contentRef.current.offsetHeight);
@@ -29,8 +33,8 @@ export default function SeasonalSection({
       break;
   }
 
-  return <>
+  return <section>
     {seasonEffect}
     <div ref={contentRef}>{children}</div>
-  </>;
+  </section>;
 }
--- src/components/seasonal-effects/Seasons.ts
+++ src/components/seasonal-effects/Seasons.ts
@@ -1,3 +1,6 @@
 export enum Seasons {
   Fall,
+  Winter,
+  Spring,
+  Summer,
 }
